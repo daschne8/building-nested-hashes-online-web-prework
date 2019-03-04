4b186e0f7dfdbc108225072c5d0981fe2e031f0f
Daniel Schneider <daschne8@gmail.com>
2019-03-04 14:43:48 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,12 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+     :montague => {
+       :patriarch => {},
+       :matriarch => {},
+       :hero => {},
+       :friends => {},
+       :hero_friends => []
+     },
+     :capulet => {}
+   }
 end
